1aa91cd3c6a7f9b0c4b8c9cc3686a0399b4c8283
nikoders5 <52191620+nikoders5@users.noreply.github.com>
2019-08-13 09:51:48 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+x=10            #NOT x = 10 no spaces
+X=20            #variables are case sensitive
+$y=             #NULL variable
+z=47
+echo "x = $x"
+echo "X = $X"
+echo "y = $y"
+echo "z = $z"
